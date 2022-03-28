90cabe523c824166aab98cd318014395f1a5ef26
Felix Kästner <mail@felix-kaestner.com>
2022-03-28 21:06:20 +0200

    add Fold, OnSuccess, OnFailure and Recover methods

--- result.go
+++ result.go
@@ -19,6 +19,24 @@ type Result[T any] interface {
 	// IsFailure returns true if this instance represents
 	// failure or false if it is success.
 	IsFailure() bool
+
+	// Fold executes the given functions when the instance
+	// represents either a success or failure respectively.
+	Fold(func(T), func(error))
+
+	// OnSuccess executes the given function if the instance
+	// represents a success.
+	OnSuccess(func(T))
+
+	// OnFailure executes the given function if the instance
+	// represents a failure.
+	OnFailure(func(error))
+
+	// Recover executes the given function if the instance
+	// represents a failure.
+	// The encapsulated value will be set to the return value
+	// of the function and the error to nil.
+	Recover(func(error) T)
 }
 
 // result is an implementation of the Result interface.
@@ -44,6 +62,32 @@ func (r *result[T]) IsSuccess() bool {
 
 func (r *result[T]) IsFailure() bool {
 	return r.err != nil
+}
+
+func (r *result[T]) Fold(onSuccess func(T), onFailure func(error)) {
+	if r.IsSuccess() {
+		onSuccess(r.value)
+	} else {
+		onFailure(r.err)
+	}
+}
+
+func (r *result[T]) OnSuccess(fn func(T)) {
+	if r.IsSuccess() {
+		fn(r.value)
+	}
+}
+
+func (r *result[T]) OnFailure(fn func(error)) {
+	if r.IsFailure() {
+		fn(r.err)
+	}
+}
+
+func (r *result[T]) Recover(fn func(error) T) {
+	if r.IsFailure() {
+		r.value, r.err = fn(r.err), nil
+	}
 }
 
 // Success creates a new Result instance representing a success.
--- result_test.go
+++ result_test.go
@@ -41,6 +41,21 @@ func TestResult(t *testing.T) {
 		assertEqual(t, nil, r.Error())
 		assertEqual(t, true, r.IsSuccess())
 		assertEqual(t, false, r.IsFailure())
+		r.Fold(func(value int) {
+			assertEqual(t, 1, value)
+		}, func(e error) {
+			t.Errorf("Expected no error, but got %v", e)
+		})
+		r.OnSuccess(func(value int) {
+			assertEqual(t, 1, value)
+		})
+		r.OnFailure(func(e error) {
+			t.Errorf("Expected no error, but got %v", e)
+		})
+		r.Recover(func(e error) int {
+			t.Errorf("Expected no error, but got %v", e)
+			return 0
+		})
 	}
 	{
 		v := 1
@@ -50,6 +65,23 @@ func TestResult(t *testing.T) {
 		assertEqual(t, nil, r.Error())
 		assertEqual(t, true, r.IsSuccess())
 		assertEqual(t, false, r.IsFailure())
+		r.Fold(func(value *int) {
+			assertEqual(t, 1, *value)
+			assertEqual(t, &v, value)
+		}, func(e error) {
+			t.Errorf("Expected no error, but got %v", e)
+		})
+		r.OnSuccess(func(value *int) {
+			assertEqual(t, 1, *value)
+			assertEqual(t, &v, value)
+		})
+		r.OnFailure(func(e error) {
+			t.Errorf("Expected no error, but got %v", e)
+		})
+		r.Recover(func(e error) *int {
+			t.Errorf("Expected no error, but got %v", e)
+			return &v
+		})
 	}
 	{
 		err := errors.New("error")
@@ -58,5 +90,21 @@ func TestResult(t *testing.T) {
 		assertEqual(t, err, r.Error())
 		assertEqual(t, false, r.IsSuccess())
 		assertEqual(t, true, r.IsFailure())
+		r.Fold(func(value any) {
+			t.Errorf("Expected error, but got value %v", value)
+		}, func(e error) {
+			assertEqual(t, err, e)
+		})
+		r.OnSuccess(func(value any) {
+			t.Errorf("Expected error, but got value %v", value)
+
+		})
+		r.OnFailure(func(e error) {
+			assertEqual(t, err, e)
+		})
+		r.Recover(func(e error) any {
+			assertEqual(t, err, e)
+			return 1
+		})
 	}
 }
